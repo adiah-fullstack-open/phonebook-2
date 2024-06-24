9a97a9c254a1463fa4a4c677e1f814d3741350da
nathanieladiah <nathanieladiah@gmail.com>
2024-06-24 09:07:56 -0400

    Add cors for using frontend

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.PORT
+    ? `http://localhost:${PORT}`
+    : "http://localhost:5173",
+  optionSuccessStatus: 200,
+};
 
 morgan.token("postData", (req, res) => JSON.stringify(req.body));
 
@@ -32,6 +40,7 @@ const loggingFunction = (tokens, req, res) => {
   }
 };
 
+app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(morgan("tiny"));
 app.use(morgan(loggingFunction));
