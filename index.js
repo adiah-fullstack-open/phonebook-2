f38d050234ac9ad19ebf77ae8a93a7fa80671395
nathanieladiah <nathanieladiah@gmail.com>
2024-06-24 08:24:29 -0400

    Add error handling for new contacts and morgan logging middleware

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
+morgan.token("postData", (req, res) => JSON.stringify(req.body));
+
+// const requestLogger = (request, response, next) => {
+//   console.log("Method: ", request.method);
+//   console.log("Path: ", request.path);
+//   console.log("Body: ", request.body);
+//   console.log("---");
+//   next();
+// };
+
+const loggingFunction = (tokens, req, res) => {
+  let message = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+  ].join(" ");
+
+  // use POST data only if it's POST method
+  if (req.method === "POST") {
+    return `${message} ${tokens.postData(req, res)}`;
+  } else {
+    return message;
+  }
+};
+
 app.use(express.json());
+// app.use(morgan("tiny"));
+app.use(morgan(loggingFunction));
+// app.use(requestLogger);
 
 let contacts = [
   {
@@ -26,6 +60,9 @@ let contacts = [
   },
 ];
 
+const checkDuplicates = (name) =>
+  contacts.filter((person) => person.name === name).length > 0;
+
 app.get("/info", (request, response) => {
   const count = contacts.length;
   const date = new Date();
@@ -41,6 +78,18 @@ app.get("/api/persons", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number missing",
+    });
+  }
+
+  if (checkDuplicates(body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
   const contact = {
     id: Math.floor(Math.random() * 1000000),
@@ -69,6 +118,12 @@ app.delete("/api/persons/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
